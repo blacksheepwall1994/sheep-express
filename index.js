88b39c413120d207b819dad124857fad76de72fb
blacksheepwall1994 <blacksheepwall1994@gmail.com>
2022-05-08 04:46:17 +0700

    change port

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use('/', (rep, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
-app.listen(80, () => {
-    console.log('Server is running on port 80');
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
 });
 
 
